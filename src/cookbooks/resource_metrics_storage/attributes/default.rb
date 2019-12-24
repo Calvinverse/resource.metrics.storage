5cea562b52fd44315516f2d124aab7a009a99994
Petrik van der Velde <petrikvandervelde@gmail.com>
2019-12-24 22:07:52 +1300

    Update to the latest version of influx

--- src/cookbooks/resource_metrics_storage/attributes/default.rb
+++ src/cookbooks/resource_metrics_storage/attributes/default.rb
@@ -27,7 +27,7 @@ default['firewall']['ipv6_enabled'] = false
 # INFLUXDB
 #
 
-default['influxdb']['version'] = '1.7.2-1'
+default['influxdb']['version'] = '1.7.9-1'
 
 default['influxdb']['lib_file_path'] = '/srv/influxdb'
 default['influxdb']['meta_file_path'] = "#{node['influxdb']['lib_file_path']}/meta"
